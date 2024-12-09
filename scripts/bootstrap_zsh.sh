5d25042447614390d542bad24108936c719cd542
rageblob <39024407+rageblob@users.noreply.github.com>
2024-12-09 10:40:21 +0100

    feat: bootstrap system

---
+++ scripts/bootstrap_zsh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create zsh config folder if it doesn't exist
+# otherwise create a backup
+if ! [[ -d "$HOME/.config/zsh" ]]; then
+  mkdir -p $HOME/.config/zsh/
+else
+  mv $HOME/.config/zsh{,.bak}
+fi
+
+# link the config files to `~/.config/zsh/`
+ln $PWD/cfg/shell/aliases $HOME/.config/zsh/aliases
+ln $PWD/cfg/shell/env $HOME/.config/zsh/env
+ln $PWD/cfg/shell/zsh_cfg $HOME/.config/zsh/settings
